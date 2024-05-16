2dab91fe987366cfe6e5dab750e65491f0d9f292
Yahwthani <0246038@up.edu.mx>
2024-05-15 20:23:48 -0600

    Grafo Bidireccional
    Grafo que encuentra la ruta mas corta en el mapa, dando dos puntos; por el momento solo pinta la ruta con gizmo, la ruta se actualiza con forme pase diferentes intersecciones en el mapa, cuando llega a el destino se imprime un mensaje.

---
+++ Assets/MapRouting/Graph.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    
+    [SerializeField]
+    private Node start;
+    [SerializeField]
+    private Node end;
+
+    List<Node> path;
+
+    
+    void Start()
+    {
+        path = GetRoute(new List<Node>(), start);
+
+        for(int i = 0; i < path.Count; i++){
+            path[i].changeStatus("path");
+        }
+
+        start.changeStatus("start");
+        end.changeStatus("end");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    List<Node> GetRoute(List<Node> visited, Node point)
+    {
+        List<Node> route = new List<Node>();
+        List<Node> newVisited = new List<Node>(visited);
+        route.Add(point);
+        if (point == end)
+        {
+            return route;
+        }
+        newVisited.Add(point);
+
+        bool stuck = true;
+        float minDis = -1.0f;
+
+        float distance;
+
+        List<Node> nextPath = new List<Node>();
+        for (int i = 0; i < point.Neighbors.Length; i++)
+        {
+            Node next = point.Neighbors[i];
+
+            if (!visited.Contains(next))
+            {
+                stuck = false;
+                List<Node> p = GetRoute(newVisited, next);
+
+                if (p.Count <= 0) continue;
+
+                distance = pathDistance(p);
+                if (minDis == -1 || distance < minDis)
+                {
+                    nextPath = p;
+                    minDis = distance;
+                }
+            }
+        }
+
+        if (stuck)
+            return new List<Node>();
+
+        if (nextPath.Count <= 0)
+        {
+            return new List<Node>();
+        }
+
+        for (int i = 0; i < nextPath.Count; i++)
+        {
+            route.Add(nextPath[i]);
+        }
+        return route;
+    }
+
+    float pathDistance(List<Node> route){
+        float sum = 0.0f;
+        for(int i = 1; i < route.Count; i++){
+            Vector3 distance = route[i].getPosition() - route[i -1].getPosition();
+            sum += distance.magnitude;
+        }
+
+        return sum;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+
+        for (int i = 0; i < path.Count - 1; i++)
+        {
+            Vector3 dir = path[i + 1].getPosition() - path[i].getPosition();
+            Gizmos.DrawRay(path[i].getPosition(), dir);
+        }
+    }
+
+    public void RecalculateRoute(Node newStart)
+    {
+        start = newStart;
+        path = GetRoute(new List<Node>(), start);
+
+        for (int i = 0; i < path.Count; i++)
+        {
+            path[i].changeStatus("path");
+        }
+        if (start.name == end.name)
+        {
+            Debug.Log("Llegaste a la meta");
+        }
+        start.changeStatus("start");
+        end.changeStatus("end");
+    }
+}
